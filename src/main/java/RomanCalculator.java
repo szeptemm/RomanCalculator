102cfa35f3246136c29ad54ee0a8247456f6d951
Robert Podsiadly <rob.podsiadly@gmail.com>
2020-03-27 08:38:41 +0100

    pair programming with Dorota, session 1 27/03/2020

--- src/main/java/RomanCalculator.java
+++ src/main/java/RomanCalculator.java
@@ -5,8 +5,38 @@ public class RomanCalculator {
     }
 
     int roman2Arabic(String roman) {
-        return 0;
+
+        char[] romanChars = roman.toCharArray();
+        int romanLen = romanChars.length;
+        int arabic = 0;
+
+        if (romanChars[0] == 'I' && romanChars.length == 1) {
+            return 1;
+        } else if ("II".equals(roman)) {
+            return 2;
+        } else if ("III".equals(roman)) {
+            return 3;
+        } else if ("X".equals(roman)) {
+            return 10;
+        } else {
+            int previosChar = 0;
+            arabic = 0;
+            for (int i = romanLen - 1; i >= 0; i--) {
+                if ('V' == romanChars[i]) {
+                    arabic = 5;
+                    previosChar = 5;
+                }
+                if ('I' == romanChars[i]) {
+                    if (previosChar != 0) {
+                        arabic -= 1;
+                    }
+                }
+            }
+        }
+        ;
+        return arabic;
     }
+
 
     String arabic2Roman(int arabic) {
         return "";
--- src/test/java/RomanCalculatorTest.java
+++ src/test/java/RomanCalculatorTest.java
@@ -1,17 +1,62 @@
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RomanCalculatorTest {
 
+    private RomanCalculator calculator;
+
+    @BeforeEach
+    void setUp(){
+        calculator = new RomanCalculator();
+    }
+
     @Test
     void shouldIGive1() {
-        // given
-        RomanCalculator calculator = new RomanCalculator();
         // when
         String roman = "I";
         // then
         assertThat( calculator.roman2Arabic(roman)).isEqualTo(1);
+    }
+
+    @Test
+    void shouldIGive2() {
+        // when
+        String roman = "II";
+        // then
+        assertThat( calculator.roman2Arabic(roman)).isEqualTo(2);
+    }
+
+    @Test
+    void shouldIGive3() {
+        // when
+        String roman = "III";
+        // then
+        assertThat( calculator.roman2Arabic(roman)).isEqualTo(3);
+    }
+
+    @Test
+    void shouldIGive5() {
+        // when
+        String roman = "V";
+        // then
+        assertThat( calculator.roman2Arabic(roman)).isEqualTo(5);
+    }
+
+    @Test
+    void shouldIGive4() {
+        // when
+        String roman = "IV";
+        // then
+        assertThat( calculator.roman2Arabic(roman)).isEqualTo(4);
+    }
+
+    @Test
+    void shouldIGive10() {
+        // when
+        String roman = "X";
+        // then
+        assertThat( calculator.roman2Arabic(roman)).isEqualTo(10);
     }
 }
